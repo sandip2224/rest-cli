2d8418a0552fb72e250dad483648260daecc3615
sandip2224 <sandipan2224@gmail.com>
2021-12-21 21:02:57 +0530

    🚧 Configured server.js for todo API

--- api/server.js
+++ api/server.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+const express = require("express")
+const colors = require("colors")
+require("dotenv").config({ path: "./.env" })
+
+const connectDB = require('./config/db')
+const app = express()
+
+// Connect to database
+connectDB()
+
+app.use(express.json())
+
+// Mounting routes
+app.use('/api', require('./routes/todoRoute'))
+
+const server = app.listen(process.env.PORT || 3000, console.log(`API running on port ${process.env.PORT || 3000}`.green.bold))
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    server.close(() => process.exit(1));
+})
